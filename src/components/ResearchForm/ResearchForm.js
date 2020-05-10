4ed930bb8d35250d6562decd2b51ab959369a7a4
Alexandru Sontica <alexandru.sontica@thalesgroup.com>
2020-05-10 15:49:51 +0300

    components are filled automatically

--- src/components/ResearchForm/ResearchForm.js
+++ src/components/ResearchForm/ResearchForm.js
@@ -10,8 +10,6 @@ import {
     Radio,
     Select,
     Checkbox,
-    Row,
-    Col,
     Input
 } from "antd";
 
@@ -26,19 +24,131 @@ const formItemLayout = {
         span: 6,
     },
     wrapperCol: {
-        span: 14,
+        span: 8,
     },
 };
 
 const ResearchForm = () => {
+
+    const defaultRequiredMessage = "Please select an answer";
 
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
     };
 
     const data = [
+        {
+            type: "yes-no",
+            question: "Va place pizza?",
+            required: true,
+            requiredMessage: "Please select an answer",
+            yesText: "Da",
+            noText:"Nu",
+        },
+        {
+            type: "range",
+            question: "Uneori cand sunt trist, ascult muzica",
+            required: true,
+            requiredMessage: "Va rugam, selectati un raspuns",
+            range: 5,
+            answers: {
+                1: "Dezacord puternic",
+                3: "Neutru",
+                5: "Acord puternic",
+            },
+        },
+        {
+            type: "multipleChoice",
+            question: "Imi place saaa",
+            answers: [
+                "The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles.",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+            ],
+        },
+        {
+            type: "text",
+            question: "Ce parere aveti de Tudor Gheorghe?",
+        },
+        {
+            type: "singleChoice",
+            question: "Cand ati fost ultima oara la un concert?",
+            required: true,
+            answers: [
+                "1 an",
+                "2 ani",
+                "3 ani",
+                "4 ani",
+            ],
+        }
 
     ];
+
+    const getSelectOptions = (range, answers) => {
+        let options = [];
+
+        for (let i = 1; i <= range; i++) {
+            if (answers[i] !== undefined) {
+                let option = (
+                    <Select.Option value={i.toString()}>{i.toString() + " " + answers[i]}</Select.Option>
+                );
+                options.push(option);
+            } else {
+                let option = (
+                    <Select.Option value={i.toString()}>{i.toString()}</Select.Option>
+                );
+                options.push(option);
+            }
+        }
+        return options;
+    };
+
+    const getCheckboxValues = (answers) => {
+        let values = [];
+        let size = answers.length;
+        let br = (<br/>);
+
+        for (let i = 0; i < size; i++) {
+            let value = (
+                <Checkbox value={i.toString()} style={{ lineHeight: '32px' }}>
+                    {answers[i]}
+                </Checkbox>
+            );
+            values.push(value);
+            values.push(br);
+        }
+
+        return values;
+    };
+
+    const getRadioValues = (answers) => {
+        let values = [];
+        let size = answers.length;
+        let br = (<br/>);
+
+        for (let i = 0; i < size; i++) {
+            let value = (
+                <Radio value={i.toString()}>
+                    {answers[i]}
+                </Radio>
+            );
+            values.push(value);
+            values.push(br);
+        }
+
+        return values;
+    };
+
+    const getRule = (required, requiredMessage) => {
+        return {
+            required: (required !== undefined && required === true),
+            message: (requiredMessage !== undefined
+                ? requiredMessage : defaultRequiredMessage)
+        };
+    };
 
     return (
         <div>
@@ -47,93 +157,91 @@ const ResearchForm = () => {
                 {...formItemLayout}
                 onFinish={onFinish}
             >
-                <Form.Item
-                    name="1"
-                    label="Va place pizza?"
-                    rules={[{ required: true, message: 'Please select an answer' }]}
-                >
-                    <Radio.Group>
-                        <Radio value="yes">Da</Radio>
-                        <Radio value="no">Nu</Radio>
-                    </Radio.Group>
-                </Form.Item>
+                {data.map((element, index) =>
+                    {
+                        if (element.type === "yes-no") {
+                            return (
+                                <Form.Item
+                                    name={index.toString()}
+                                    label={element.question}
+                                    rules={[
+                                        getRule(element.required, element.requiredMessage)
+                                    ]}
+                                >
+                                    <Radio.Group>
+                                        <Radio value="yes">{element.yesText}</Radio>
+                                        <Radio value="no">{element.noText}</Radio>
+                                    </Radio.Group>
 
-                <Form.Item
-                    name="2"
-                    label="Uneori cand sunt trist, ascult muzica"
-                    rules={[{ required: true, message: 'Please select an answer' }]}
-                    hasFeedback
-                >
-                    <Select>
-                        <Select.Option value="1">1 Dezacord puternic</Select.Option>
-                        <Select.Option value="2">2</Select.Option>
-                        <Select.Option value="3">3</Select.Option>
-                        <Select.Option value="4">4</Select.Option>
-                        <Select.Option value="5">5 Acord puternic</Select.Option>
-                    </Select>
+                                </Form.Item>
+                            );
 
-                </Form.Item>
+                        } else if (element.type === "range") {
+                            return (
+                                <Form.Item
+                                    name={index.toString()}
+                                    label={element.question}
+                                    rules={[
+                                        getRule(element.required, element.requiredMessage)
+                                    ]}
+                                    hasFeedback
+                                    wrapperCol={{
+                                        span: 3,
+                                    }}
+                                >
+                                    <Select>
+                                        {getSelectOptions(element.range, element.answers)}
+                                    </Select>
 
-                <Form.Item
-                    name="3"
-                    label="Imi place sa"
-                    rules={[{ required: true, message: 'Please select an answer' }]}
-                >
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={2}>
-                                <Checkbox value="A" style={{ lineHeight: '32px' }}>
-                                    A
-                                </Checkbox>
-                            </Col>
-                            <Col span={2}>
-                                <Checkbox value="B" style={{ lineHeight: '32px' }}>
-                                    B
-                                </Checkbox>
-                            </Col>
-                            <Col span={2}>
-                                <Checkbox value="C" style={{ lineHeight: '32px' }}>
-                                    C
-                                </Checkbox>
-                            </Col>
-                            <Col span={2}>
-                                <Checkbox value="D" style={{ lineHeight: '32px' }}>
-                                    D
-                                </Checkbox>
-                            </Col>
-                            <Col span={2}>
-                                <Checkbox value="E" style={{ lineHeight: '32px' }}>
-                                    E
-                                </Checkbox>
-                            </Col>
-                            <Col span={2}>
-                                <Checkbox value="F" style={{ lineHeight: '32px' }}>
-                                    F
-                                </Checkbox>
-                            </Col>
-                        </Row>
-                    </Checkbox.Group>
-
-                </Form.Item>
-
-                <Form.Item
-                    name="4"
-                    label="Ce parere aveti de Tudor Gheorghe?"
-                >
-                    <Input.TextArea rows={4}/>
-                </Form.Item>
-
-                {/*<Form.List name="research_form_list">*/}
-                {/*    {fields => (*/}
-                {/*        <div>*/}
-                {/*            {fields.map(field => (*/}
-                {/*                <Form.Item {...field}>*/}
-                {/*                    <div></div>*/}
-                {/*                </Form.Item>*/}
-                {/*            ))}*/}
-                {/*        </div>*/}
-                {/*    )}*/}
-                {/*</Form.List>*/}
+                                </Form.Item>
+                            );
+                        } else if (element.type === "multipleChoice") {
+                            return (
+                                <Form.Item
+                                    name={index.toString()}
+                                    label={element.question}
+                                    rules={[
+                                        getRule(element.required, element.requiredMessage)
+                                    ]}
+                                >
+                                    <Checkbox.Group>
+                                        {getCheckboxValues(element.answers)}
+                                    </Checkbox.Group>
+                                </Form.Item>
+                            );
+                        } else if (element.type === "text") {
+                            return (
+                                <Form.Item
+                                    name={index.toString()}
+                                    label={element.question}
+                                    rules={[
+                                        getRule(element.required, element.requiredMessage)
+                                    ]}
+                                >
+                                    <Input.TextArea rows={4}/>
+                                </Form.Item>
+                            );
+                        } else if (element.type === "singleChoice") {
+                            return (
+                                <Form.Item
+                                    name={index.toString()}
+                                    label={element.question}
+                                    rules={[
+                                        getRule(element.required, element.requiredMessage)
+                                    ]}
+                                >
+                                    <Radio.Group>
+                                        {getRadioValues(element.answers)}
+                                    </Radio.Group>
+                                </Form.Item>
+                            );
+                        } else {
+                            return (
+                                <div></div>
+                            );
+                        }
+                    }
+                )}
 
                 <Form.Item
                     wrapperCol={{
